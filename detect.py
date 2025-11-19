48e85775f6e3327d88bde0f664271f743f111c7c
Alin1013 <2253736501@qq.com>
2025-11-19 17:02:22 +0800

    训练模块修改，半训练模型修改

--- detect.py
+++ detect.py
@@ -8,36 +8,35 @@ from get_yaml import get_config
 from yolo import YOLO
 from gen_annotation import GEN_Annotations
 
-if __name__ == "__main__":# 配置文件
-    # 配置文件
+if __name__ == "__main__":
+    # 加载配置
     config = get_config()
     yolo = YOLO()
 
     dir_detect_path = config['dir_detect_path']
-    detect_save_path   = config['detect_save_path']
-    
+    detect_save_path = config['detect_save_path']
+
     import os
     from tqdm import tqdm
-    
+
     img_names = os.listdir(dir_detect_path)
     for img_name in tqdm(img_names):
-        
-        if img_name.lower().endswith(('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
-            # if int(img_name.split('.')[0][-4:]) < 355:
-            #     continue
-            image_path  = os.path.join(dir_detect_path, img_name)
-            image       = Image.open(image_path)
-            boxes       = yolo.get_box(image)
+        # 处理图像文件
+        if img_name.lower().endswith(
+                ('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
+            image_path = os.path.join(dir_detect_path, img_name)
+            image = Image.open(image_path)
+            boxes = yolo.get_box(image)  # 获取检测框
             if not os.path.exists(detect_save_path):
                 os.makedirs(detect_save_path)
 
-            annotation        = GEN_Annotations(img_name)
-            w,h = np.array(np.shape(image)[0:2])
-            annotation.set_size(w,h,3)
+            # 生成标注文件
+            annotation = GEN_Annotations(img_name)
+            w, h = np.array(np.shape(image)[0:2])
+            annotation.set_size(w, h, 3)
             if boxes:
                 for box in boxes:
-                    label,ymin,xmin,ymax,xmax = box
-                    annotation.add_pic_attr(label,xmin,ymin,xmax,ymax)
-                annotation_path  = os.path.join(detect_save_path, img_name.split('.')[0])
-                annotation.savefile("{}.xml".format(annotation_path ))
-            # print(img_name,'已经半自动标注')
+                    label, ymin, xmin, ymax, xmax = box
+                    annotation.add_pic_attr(label, xmin, ymin, xmax, ymax)
+                annotation_path = os.path.join(detect_save_path, img_name.split('.')[0])
+                annotation.savefile("{}.xml".format(annotation_path))
--- train.py
+++ train.py
@@ -2,6 +2,8 @@
 import argparse
 from ultralytics import YOLO
 from get_yaml import get_config
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
