d3d38899e758426354fd0bde87455892d23aff8d
Alin1013 <2253736501@qq.com>
2025-11-19 17:28:45 +0800

    删除 lr_decay_type 参数

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument('--imgsz', type=int, default=640, help='输入图像尺寸')
     parser.add_argument('--weights', '-w', type=str, default='yolov8n.pt', help='初始权重路径')
     parser.add_argument('--data', type=str, default='model_data/gesture.yaml', help='数据集配置文件')
-    parser.add_argument('--device', type=str, default='0', help='设备，0为GPU，cpu为CPU')
+    parser.add_argument('--device', type=str, default='cpu', help='设备，0为GPU，cpu为CPU（Mac建议用cpu）')
     parser.add_argument('--freeze', action='store_true', help='冻结训练')
     parser.add_argument('--freeze-epochs', type=int, default=50, help='冻结训练轮数')
     parser.add_argument('--lr0', type=float, default=0.01, help='初始学习率')
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     if freeze:
         # 冻结主干网络
         model.freeze()
-        # 先进行冻结训练
+        # 先进行冻结训练（删除 lr_decay_type 参数）
         model.train(
             data=opt.data,
             epochs=opt.freeze_epochs,
@@ -35,6 +35,7 @@ if __name__ == "__main__":
             batch=opt.batch_size,
             device=opt.device,
             lr0=opt.lr0,
+            workers=4,  # 对应yaml中的workers配置
             name='freeze_train'
         )
         # 解冻所有层
@@ -49,11 +50,12 @@ if __name__ == "__main__":
                 batch=opt.batch_size,
                 device=opt.device,
                 lr0=opt.lr0/10,  # 学习率减半
+                workers=4,
                 name='unfreeze_train',
                 resume=True
             )
     else:
-        # 正常训练
+        # 正常训练（删除 lr_decay_type 参数）
         model.train(
             data=opt.data,
             epochs=opt.epochs,
@@ -61,5 +63,6 @@ if __name__ == "__main__":
             batch=opt.batch_size,
             device=opt.device,
             lr0=opt.lr0,
+            workers=4,
             name='normal_train'
         )
